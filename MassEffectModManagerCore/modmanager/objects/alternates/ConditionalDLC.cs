a821d3aed7109bf57c437d9ef004a58160332e8d
Mgamerz <mgamerz@msn.com>
2024-06-06 09:23:52 -0600

    ConditionalDLC class now supports subconditions MinVersion, MaxVersion to help version target perhaps old mod that was updated that users do not like like old MEHEM version differences.

--- MassEffectModManagerCore/modmanager/objects/alternates/ConditionalDLC.cs
+++ MassEffectModManagerCore/modmanager/objects/alternates/ConditionalDLC.cs
@@ -1,4 +1,6 @@
-﻿using ME3TweaksCore.Helpers;
+﻿using LegendaryExplorerCore.Misc;
+using ME3TweaksCore.Helpers;
+using ME3TweaksCore.Objects;
 using ME3TweaksModManager.modmanager.objects.mod;
 
 namespace ME3TweaksModManager.modmanager.objects.alternates
@@ -8,6 +10,10 @@ namespace ME3TweaksModManager.modmanager.objects.alternates
     /// </summary>
     public class ConditionalDLC
     {
+        private const string REQKEY_DLCOPTIONKEY = @"optionkey";
+        private const string REQKEY_MINVERSION = @"minversion";
+        private const string REQKEY_MAXVERSION = @"maxversion"; // I highly doubt this will be used, but maybe some new mod will make people unhappy and they will try to use old version. DLCRequirements does not support this as it doesn't really make sense to only allow install against older version
+
         /// <summary>
         /// DLC folder name
         /// </summary>
@@ -15,30 +21,74 @@ namespace ME3TweaksModManager.modmanager.objects.alternates
 
         public List<PlusMinusKey> OptionKeyDependencies { get; set; } = new(0);
 
+        public Version MinVersion { get; set; }
+        public Version MaxVersion { get; set; }
+
         public ConditionalDLC(Mod mod, string input, bool canBePlusMinus = false)
         {
             var dlcName = input;
             DLCName = canBePlusMinus ? new PlusMinusKey(dlcName) : new PlusMinusKey(null, input);
             if (mod.ModDescTargetVersion >= 9.0)
             {
-                var openParenPos = input.IndexOf('(');
-                var closeParenPos = input.IndexOf(')');
+                if (input.Contains('(') || input.Contains(')'))
+                    throw new Exception("ConditionalDLC cannot contain ( or ) due to parser issues that would potentially break backwards compatibility if fixed. Use [ ] instead for conditions.");
+
+
+                var openParenPos = input.IndexOf('[');
+                var closeParenPos = input.IndexOf(']'); // Last Index of?
                 if (openParenPos > 0 && closeParenPos == -1)
                 {
-                    throw new Exception($"Conditional DLC {input} contains opening parenthesis but no closing parenthesis.");
+                    throw new Exception($"ConditionalDLC {input} contains opening bracket but no closing bracket.");
                 }
 
                 if (openParenPos > 0)
                 {
                     DLCName.Key = DLCName.Key.Substring(0, openParenPos);
-                    var conditionalDLCStruct = input.Substring(openParenPos, closeParenPos + 1);
-                    var structKeys = StringStructParser.GetCommaSplitList(conditionalDLCStruct);
-                    foreach (var key in structKeys)
+                    var conditionalDLCStruct = input.Substring(openParenPos, closeParenPos - openParenPos + 1);
+                    var structKeys = StringStructParser.GetSplitMultiValues(conditionalDLCStruct);
+                    foreach (var param in structKeys)
                     {
-                        OptionKeyDependencies.Add(new PlusMinusKey(key));
+                        switch (param.Key)
+                        {
+                            case ConditionalDLC.REQKEY_DLCOPTIONKEY when mod.ModDescTargetVersion >= 9.0: // This is gated above, but this is for if we add more to the switch statement.
+                                OptionKeyDependencies.AddRange(param.Value.Select(x => new PlusMinusKey(x)));
+                                break;
+                            case ConditionalDLC.REQKEY_MINVERSION when mod.ModDescTargetVersion >= 9.0: // This is gated above, but this is for if we add more to the switch statement.
+                                OptionKeyDependencies.AddRange(param.Value.Select(x => new PlusMinusKey(x)));
+                                break;
+                            case ConditionalDLC.REQKEY_MAXVERSION when mod.ModDescTargetVersion >= 9.0: // This is gated above, but this is for if we add more to the switch statement.
+                                OptionKeyDependencies.AddRange(param.Value.Select(x => new PlusMinusKey(x)));
+                                break;
+                        }
                     }
                 }
             }
+        }
+
+
+        /// <summary>
+        /// ToString() for this. This is what gets serialized into ModDesc!
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (!HasConditions())
+                return DLCName.ToString();
+
+            CaseInsensitiveDictionary<List<string>> keyMap = new CaseInsensitiveDictionary<List<string>>();
+            if (MinVersion != null)
+                keyMap[REQKEY_MINVERSION] = [MinVersion.ToString()];
+            if (MaxVersion != null)
+                keyMap[REQKEY_MAXVERSION] = [MaxVersion.ToString()];
+            if (OptionKeyDependencies != null)
+                keyMap[REQKEY_DLCOPTIONKEY] = OptionKeyDependencies.Select(x => x.ToString()).ToList();
+
+            return $@"{DLCName}{StringStructParser.BuildCommaSeparatedSplitMultiValueList(keyMap, '[', ']')}";
+        }
+
+        private bool HasConditions()
+        {
+            return MinVersion != null || MaxVersion != null || OptionKeyDependencies.Any();
         }
     }
 }
