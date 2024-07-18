3295e3d746a1e270bbc2fa76019fec9f6de085cc
Mgamerz <mgamerz@msn.com>
2024-07-18 09:26:12 -0600

    SevenZipSharp: Final progress event will now occur

--- SevenZip/ArchiveExtractCallback.cs
+++ SevenZip/ArchiveExtractCallback.cs
@@ -254,7 +254,7 @@ namespace SevenZip
         /// <param name="completeValue">Amount of work that has been completed (in bytes)</param>
         public void SetCompleted(ref ulong completeValue)
         {
-            if (completeValue < (ulong)_bytesCount && completeValue >= 0)
+            if (completeValue <= (ulong)_bytesCount && completeValue >= 0)
             {
                 OnProgressing(new DetailedProgressEventArgs(completeValue, (ulong) _bytesCount));
             }
